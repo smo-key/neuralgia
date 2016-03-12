41f8e15fe2c25e9c3b6bf79cfe3b737437fa1cf1
Arthur Pachachura <pachachura.arthur@gmail.com>
2016-03-12 00:01:19 -0600

    Add basic neuron

--- Neuralgia/main.py
+++ Neuralgia/main.py
@@ -1,4 +1,9 @@
 from nn import struct
 
-perceptron = struct.Perceptron([1, 2], 10);
+perceptron = struct.Perceptron([1, 2], 16);
 print perceptron.sum([4, 4]);
+print perceptron.output([4, 4]);
+
+neuron = struct.Neuron([1, 2], 16);
+print neuron.sum([4, 4]);
+print neuron.output([4, 4]);
--- Neuralgia/nn/struct.py
+++ Neuralgia/nn/struct.py
@@ -1,6 +1,8 @@
+import math;
+
 class Perceptron:
-	weights = [] # weights
-	bias = 0 # bias value == -threshold
+	weights = []	# weights
+	bias = 0		# bias value == -threshold
 
 	def __init__(self, weights, bias):
 		self.weights = weights;
@@ -16,3 +18,26 @@ class Perceptron:
 		sum = self.sum(inputs)
 		if sum + self.bias > 0: return 1
 		else: return 0
+
+class Neuron:
+	sigmoid = lambda z:1 / (1 + math.exp(-z)) # sigmoid function
+	step = lambda x:1 if x > 0 else 0		  # step function
+	
+	activationFunc = sigmoid				  # activation function (interchangable)
+	weights = []	# weights
+	bias = 0		# bias value == -threshold
+
+	def __init__(self, weights, bias, activationFunc=sigmoid):
+		self.weights = weights;
+		self.bias = bias;
+		self.activationFunc = activationFunc;
+		
+	def sum(self, inputs):
+		# compute weighted sum
+		if (len(inputs) != len(self.weights)):
+			raise IndexError()
+		return reduce(lambda x,y:x+y, map(lambda x,y:x*y, self.weights, inputs));
+
+	def output(self, inputs):
+		sum = self.sum(inputs)
+		return self.activationFunc(sum);
